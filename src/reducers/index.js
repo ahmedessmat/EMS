898e395626b98f18ed60a8a7fb16c6633409f3d7
Ahmed Amer Hassan <ahmedamerxx@gmail.com>
2020-12-17 20:26:08 +0200

    Switch to typescript and new application

--- src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import authReducer from './authReducer';
-import appReducer from './appReducer';
-
-export default combineReducers({
-  auth: authReducer,
-  app: appReducer,
-});
